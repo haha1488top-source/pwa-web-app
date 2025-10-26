69020657104e472976bc4f4519716fa9f0878682
haha1488top-source <haha1488top@gmail.com>
2025-10-26 03:33:48 +0300

    Update servis-worker.js

--- servis-worker.js
+++ servis-worker.js
@@ -1,49 +1,49 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'clicker-v2';
 const urlsToCache = [
-  'pwapp.html',
-  'manifest.json',
-  'script.js',
-  'Img/pin_dead_box.png',
-  // Додайте всі інші ресурси, які потрібні офлайн (наприклад, CSS-файли)
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json',
+  './Img/pin_dead_box.png'
 ];
 
-// 1. Подія Install: кешуємо всі необхідні ресурси
-self.addEventListener('install', (event) => {
+// Встановлення - кешуємо всі файли
+self.addEventListener('install', event => {
+  console.log('SW: Installing...');
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('SW: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
-// 2. Подія Fetch: перехоплюємо запити і віддаємо з кешу, якщо можливо
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Віддаємо з кешу, якщо знайдено
-      if (response) {
-        return response;
-      }
-      // Інакше робимо мережевий запит
-      return fetch(event.request);
-    })
-  );
-});
-
-// 3. Подія Activate: видаляємо старі кеші
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+// Активізація - видаляємо старі кеші
+self.addEventListener('activate', event => {
+  console.log('SW: Activated');
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Видаляємо старий кеш
-            return caches.delete(cacheName);
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('SW: Deleting old cache', cache);
+            return caches.delete(cache);
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch - віддаємо з кешу
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Повертаємо кеш або робимо запит
+        return response || fetch(event.request);
+      })
   );
 });
