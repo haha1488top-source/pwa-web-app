9848c4c1960dd3091b26a1525f877bb0beceffc5
haha1488top-source <haha1488top@gmail.com>
2025-10-26 03:29:17 +0300

    Update servis-worker.js

--- servis-worker.js
+++ servis-worker.js
@@ -1,1 +1,49 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  'pwapp.html',
+  'manifest.json',
+  'script.js',
+  'Img/pin_dead_box.png',
+  // Додайте всі інші ресурси, які потрібні офлайн (наприклад, CSS-файли)
+];
 
+// 1. Подія Install: кешуємо всі необхідні ресурси
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 2. Подія Fetch: перехоплюємо запити і віддаємо з кешу, якщо можливо
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Віддаємо з кешу, якщо знайдено
+      if (response) {
+        return response;
+      }
+      // Інакше робимо мережевий запит
+      return fetch(event.request);
+    })
+  );
+});
+
+// 3. Подія Activate: видаляємо старі кеші
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Видаляємо старий кеш
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
